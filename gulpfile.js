7976d95a1b8467d68206acac36ae969332e8b382
oliegjio <oliegjio@gmail.com>
2016-12-29 21:57:51 +0300

    Angular 2 Setup Finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
+require('elixir-typescript');
+
 
 /*
  |--------------------------------------------------------------------------
@@ -9,11 +11,37 @@ require('laravel-elixir-vue-2');
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
  | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
+ | file for our application, as well as publishing vendor resources.
  |
  */
 
-elixir((mix) => {
+elixir(mix => {
     mix.sass('app.scss')
-       .webpack('app.js');
+      .webpack('app.js')
+      .copy('node_modules/@angular', 'public/vendor/@angular')
+      .copy('node_modules/anular2-in-memory-web-api', 'public/vendor/anular2-in-memory-web-api')
+      .copy('node_modules/core-js', 'public/vendor/core-js')
+      .copy('node_modules/reflect-metadata', 'public/vendor/reflect-metadata')
+      .copy('node_modules/systemjs', 'public/vendor/systemjs')
+      .copy('node_modules/rxjs', 'public/vendor/rxjs')
+      .copy('node_modules/zone.js', 'public/vendor/zone.js')
+
+      .typescript(
+          [
+              'app.component.ts',
+              'app.module.ts',
+              'main.ts'
+          ],
+          'public/app',
+          {
+              "target": "es5",
+              "module": "system",
+              "moduleResolution": "node",
+              "sourceMap": true,
+              "emitDecoratorMetadata": true,
+              "experimentalDecorators": true,
+              "removeComments": false,
+              "noImplicitAny": false
+          }
+      );
 });
